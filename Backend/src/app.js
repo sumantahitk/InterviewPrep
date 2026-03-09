a9f9c7b7ebe1156580e89fec64b88b9185f6367a
Sumanta Rakshit <sumantarakshit6299@gmail.com>
2026-03-09 16:08:10 +0530

    completed the frontend part for authentication

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,13 +1,16 @@
 const express=require("express")
 const authRouter=require("./routes/auth.routes")
 const cookiesParser=require("cookie-parser")
-
+const cors=require("cors")
 
 const app=express();
 
 app.use(express.json())
 app.use(cookiesParser())
-
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentilas:true
+}))
 
 app.use("/api/auth",authRouter);
 
---
+++ Frontend/src/app.routes.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router";
+import Login from "./features/auth/pages/Login";
+import Register from "./features/auth/pages/Register";
+// import Protected from "./features/auth/components/Protected";
+// import Home from "./features/interview/pages/Home";
+// import Interview from "./features/interview/pages/Interview";
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/login",
+        element: <Login />
+    },
+    {
+        path: "/register",
+        element: <Register />
+    },
+    {
+        path: "/",
+        // element: <Protected><Home /></Protected>
+        element: <h1>Home Page</h1>
+    }
+    // {
+    //     path:"/interview/:interviewId",
+    //     element: <Protected><Interview /></Protected>
+    // }
+])
